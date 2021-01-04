4b7d32d37993c3aa7ee5d483c50fb3927bb0407e
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-01-04 19:39:56 -0300

    added: rolagem de dados por fórmula

--- controllers/RolagemController.js
+++ controllers/RolagemController.js
@@ -1,3 +1,6 @@
+const { DiceRoll } = require('rpg-dice-roller/lib/umd/bundle.js');
+const { getPericiaByLabel } = require('../data/Mapeamento');
+
 module.exports = (app) => {
 
     app.get(`/rolagem/personagem/:id`, async (req, res) => {
@@ -17,6 +20,179 @@ module.exports = (app) => {
             timestamp: new Date() / 1000 | 0,
             rolagens: rolagem
         };
+        res.send(resp);
+    });
+
+    app.get(`/teste-atributo/:atributo`, async (req, res) => {
+        const { query, params } = req;
+        const resp = {
+            status: 0,
+            data: null,
+            errors: [],
+            msg: ''
+        };
+
+        const personagem = await Personagem.GetFirst(`id = '${query.personagem}'`);
+
+        if (!personagem) {
+            resp.errors.push({
+                msg: "Personagem não encontrado"
+            });
+            return res.status(404).send(resp);
+        }
+
+        const valorAtributo = personagem[params.atributo];
+        const modificador = Personagem.calcularModificador(valorAtributo);
+        
+        const roll1 = new DiceRoll(`1d20`);
+        let valorRolagem = roll1.total;
+
+        const data = {
+            id: Util.generateId(),
+            personagem: query.personagem,
+            atributo: params.atributo,
+            titulo: params.atributo,
+            valor: valorRolagem,
+            modificador: modificador,
+            data: new Date() / 1000 | 0
+        };
+
+        const sobra = 20 - valorAtributo;
+        if (valorRolagem === 1) {
+            data.tipo = "Critico";
+        } else if (sobra > valorRolagem) {
+            data.tipo = "Falha";
+        } else if (sobra + valorAtributo * 0.5 >= valorRolagem) {
+            data.tipo = "Normal";
+        } else if (sobra + valorAtributo * 0.9 >= valorRolagem) {
+            data.tipo = "Bom";
+        } else {
+            data.tipo = "Extremo";
+        }
+
+
+        const createRolagem = await Rolagem.Create(data);
+        if (createRolagem.status !== 1) {
+            resp.errors.push({
+                msg: "Erro ao rolar os dados"
+            });
+            return res.status(500).send(resp);
+        }
+
+        resp.status = 1;
+        resp.data = data;
+
+        res.send(resp);
+    });
+
+    app.get(`/teste-pericia/:pericia`, async (req, res) => {
+        const { query, params } = req;
+        const resp = {
+            status: 0,
+            data: null,
+            errors: [],
+            msg: ''
+        };
+
+        const personagem = await Personagem.GetFirst(`id = '${query.personagem}'`);
+
+        if (!personagem) {
+            resp.errors.push({
+                msg: "Personagem não encontrado"
+            });
+            return res.status(404).send(resp);
+        }
+
+        personagem.pericias = (await Pericia.Get(`personagem = '${personagem.id}'`)) || [];
+        const pericia = getPericiaByLabel(params.pericia);
+
+        let modificador = Personagem.calcularModificador(personagem[pericia.atributo]);
+        
+        if(personagem.pericias.find(x => x.nome === pericia.nome)){
+            modificador = modificador + Personagem.calcularProficiencia(personagem.nivel);
+        }
+        
+        const valorPericia = modificador + personagem[pericia.atributo];
+
+        const roll1 = new DiceRoll(`1d20+${modificador}`);
+        let valorRolagem = roll1.total;        
+        
+        const data = {
+            id: Util.generateId(),
+            personagem: query.personagem,
+            atributo: pericia.nome,
+            titulo: params.atributo,
+            valor: valorRolagem,
+            modificador: modificador,
+            data: new Date() / 1000 | 0
+        };
+
+        const porcent = (valorRolagem / (20 + modificador)) * 100;
+
+        if(valorRolagem === 1){
+            data.tipo = "Crítico";
+        }else if (porcent < 50){
+            data.tipo = "Falha";
+        }else if(porcent >= 50 && porcent < 90){
+            data.tipo = "Normal";
+        }else{
+            data.tipo = "Extremo";
+        }
+        const createRolagem = await Rolagem.Create(data);
+        if (createRolagem.status !== 1) {
+            resp.errors.push({
+                msg: "Erro ao rolar os dados"
+            });
+            return res.status(500).send(resp);
+        }
+
+
+        resp.status = 1;
+        resp.data = data;
+        res.send(resp);
+    });
+
+    app.get(`/rolagem/rolar/:formula`, async (req, res) => {
+        const { query, params } = req;
+        const resp = {
+            status: 0,
+            data: null,
+            errors: [],
+            msg: ''
+        };
+
+        const personagem = await Personagem.GetFirst(`id = '${query.personagem}'`);
+
+        if (!personagem) {
+            resp.errors.push({
+                msg: "Personagem não encontrado"
+            });
+            return res.status(404).send(resp);
+        }
+
+        const roll1 = new DiceRoll(`${params.formula || '1d20'}`);
+        let valorRolagem = roll1.total;
+
+        const data = {
+            id: Util.generateId(),
+            personagem: query.personagem,
+            formula: params.formula,
+            valor: valorRolagem,
+            data: new Date() / 1000 | 0
+        };
+
+        const createRolagem = await Rolagem.Create(data);
+        if (createRolagem.status !== 1) {
+            resp.errors.push({
+                msg: "Erro ao rolar os dados"
+            });
+            return res.status(500).send(resp);
+        }
+
+        resp.status = 1;
+        resp.data = data;
+
+        resp.data = data;
         res.send(resp);
     });
 
