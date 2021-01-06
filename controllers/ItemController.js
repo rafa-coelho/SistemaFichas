6f0b7d29e2f9b6a8dfd6f77900dfc03f19c654b8
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-01-06 01:34:51 -0300

    added: Editar item

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -1,5 +1,57 @@
 module.exports = (app) => {
 
+    app.put(`/item/:id`, async (req, res) => {
+        const { params, body } = req;
+        const resp = {
+            status: 0,
+            data: null,
+            errors: [],
+            msg: ''
+        };
+
+        const item = await Item.GetFirst(`id = '${params.id}'`);
+
+        if (!item) {
+            resp.errors.push({
+                msg: "Item não encontrado"
+            });
+            return res.status(404).send(resp);
+        }
+
+        const proibidos = ['id'];
+
+        let edit = false;
+
+        const payload = {};
+        Item.fields.forEach(campo => {
+            if (body[campo] !== undefined && !proibidos.includes(campo)) {
+                payload[campo] = body[campo];
+                edit = true;
+            }
+        });
+
+        if (!edit) {
+            resp.errors.push({
+                msg: "Nada para editar"
+            });
+            return res.status(400).send(resp);
+        }
+
+        const update = await Item.Update(payload, `id = '${params.id}'`);
+
+        if (update.status !== 1) {
+            resp.errors.push({
+                msg: "Não foi possivel atualizar",
+            });
+
+            return res.status(500).send(resp);
+        }
+
+        resp.status = 1;
+        resp.msg = "Atualizado com sucesso";
+        res.send(resp);
+    });
+
     app.delete(`/item/:id`, async (req, res) => {
         const { params } = req;
         const resp = {
