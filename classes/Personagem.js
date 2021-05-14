90d751064646e58ee82d830bfd34795dc09bd686
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-05-14 16:04:41 -0300

    fixed: calculo de proficiencia

--- classes/Personagem.js
+++ classes/Personagem.js
@@ -1,35 +1,34 @@
-class Personagem extends Classes
-{
+class Personagem extends Classes {
 
     static table = 'personagem';
-    static fields = [ 'id', 'nome', 'raca', 'classe', 'nivel', 'hp', 'hp_maximo', 'sanidade', 'sanidade_maxima', 'forca', 'destreza', 'constituicao', 'inteligencia', 'sabedoria', 'carisma', 'dado_vida', 'npc', 'pc', 'pp', 'pe', 'po', 'pl' ];
+    static fields = ['id', 'nome', 'raca', 'classe', 'nivel', 'hp', 'hp_maximo', 'sanidade', 'sanidade_maxima', 'forca', 'destreza', 'constituicao', 'inteligencia', 'sabedoria', 'carisma', 'dado_vida', 'npc', 'pc', 'pp', 'pe', 'po', 'pl'];
 
     static calcularModificador = (valor) => {
-        let repeat = [ ];
+        let repeat = [];
         let modificador = -5;
-        for(let i = 1; i < valor; i++){
+        for (let i = 1; i < valor; i++) {
             repeat.push(modificador)
-            
-            if(repeat.filter(x => x === modificador).length == 2 || modificador === -5){
+
+            if (repeat.filter(x => x === modificador).length == 2 || modificador === -5) {
 
                 modificador = modificador + 1;
             }
         }
-        
+
         return modificador;
     }
-    
+
     static calcularProficiencia = (valor) => {
-        let repeat = [ ];
+        let repeat = [];
         let proficiencia = 2;
-        for(let i = 1; i <= valor; i++){
+        for (let i = 1; i <= valor; i++) {
             repeat.push(proficiencia)
-            
-            if(repeat.filter(x => x === proficiencia).length == 4 || proficiencia === -5){
+
+            if (repeat.filter(x => x === proficiencia).length == 4 || proficiencia === -5) {
                 proficiencia = proficiencia + 1;
             }
         }
-        
+
         return proficiencia;
     }
 
--- controllers/RolagemController.js
+++ controllers/RolagemController.js
@@ -3,20 +3,20 @@ const { getPericiaByLabel, getAtributoByLabel, getLoucura } = require('../data/M
 
 module.exports = (app) => {
 
-    app.get(`/rolagem/personagem/:id`, async (req, res) => {
-        const { params, query } = req;
-        const resp = {
-            status: 1,
-            data: null,
-            errors: [],
-            msg: ''
-        };
+        app.get(`/rolagem/personagem/:id`, async(req, res) => {
+                    const { params, query } = req;
+                    const resp = {
+                        status: 1,
+                        data: null,
+                        errors: [],
+                        msg: ''
+                    };
 
-        const timestamp = query.timestamp;
+                    const timestamp = query.timestamp;
 
-        const rolagem = await Rolagem.Get(`personagem = '${params.id}' AND data >= '${timestamp}'`, 'data DESC');
+                    const rolagem = await Rolagem.Get(`personagem = '${params.id}' AND data >= '${timestamp}'`, 'data DESC');
 
-        await Rolagem.Delete(`id in ('${rolagem.map(x => x.id).join(`', '`)}')`);
+                    await Rolagem.Delete(`id in ('${rolagem.map(x => x.id).join(`', '`)}')`);
 
         resp.data = {
             timestamp: new Date() / 1000 | 0,
@@ -126,7 +126,7 @@ module.exports = (app) => {
         const pericia = getPericiaByLabel(params.pericia);
 
         let modificador = Personagem.calcularModificador(personagem[pericia.atributo]);
-        const proficiencia = 0;
+        let proficiencia = 0;
 
         if (personagem.pericias.find(x => x.nome === pericia.nome)) {
             modificador = modificador + Personagem.calcularProficiencia(personagem.nivel);
